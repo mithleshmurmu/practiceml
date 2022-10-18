624ef15d159d868db00655234f2d2dbda1c6c51b
HDolekar <himanshudolekar1@gmail.com>
2022-10-18 22:53:57 +0530

    sample_out

--- hubconf.py
+++ hubconf.py
@@ -185,8 +185,10 @@ def load_model(mypath="model.pth"):
 def sample_test(model1, test_data):
     model1.eval()
     x, y = test_data[0]
-    with torch.no_grad():
-        pred = model1(x.cuda())
-        predicted, actual = classes[pred[0].argmax(0)], classes[y]
-        print(f'Predicted: "{predicted}", Actual: "{actual}"')
+    print(x.shape)
+    print(y.shape)
+    # with torch.no_grad():
+    #     pred = model1(x.cuda())
+    #     predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    #     print(f'Predicted: "{predicted}", Actual: "{actual}"')
 
